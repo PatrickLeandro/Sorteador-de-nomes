c11504d11763aeacc3304c2d55ada55464357d64
Patrick Leandro <patrickleandro34@gmail.com>
2020-10-26 22:01:03 -0300

    Atualização
    Atatulização de todo o projeto.
    Adicionado menu responsivo, porém ainda incompleto.
    O projeto agora conta com a opção de dark mode.
    Algumas mudanças simples no documento html.
    JavaScript foi trabalhado também.

---
+++ script/script.js
@@ -0,0 +1,51 @@
+let contador = 0;
+function teste() {
+  let menu = document.getElementById("menu-hamburguer");
+    if (contador == 0) {
+      menu.style.display = 'block';
+      contador = 1;
+      }else{
+        menu.style.display = 'none';
+        contador = 0;
+      }
+      
+    }
+
+  const $html = document.querySelector('html')
+  const $checkbox = document.querySelector('#switch')
+
+  $checkbox.addEventListener('change', function () {
+      $html.classList.toggle('dark-mode')
+  })
+
+  var sort = 0
+  var nomes = []
+
+  function text(){
+
+      var list = txt.value.split("\n")
+      
+
+      for (var i = 0; i < list.length; i++){
+          if (list[i].length > 0) {
+              sort=i+1
+              nomes[i] = list[i]
+              
+          }
+      }
+  }
+
+  
+
+  function sortear() {
+      var ran = Math.floor(Math.random() * sort);
+      
+      
+      document.getElementById("sorteados").innerHTML="O nome sorteado foi: <br>"+ nomes[ran];
+      let res = document.getElementById("resultado");
+      res.style.display = 'flex';
+  }
+  function reset() {
+    let res = document.getElementById("resultado");
+      res.style.display = 'none';
+}
